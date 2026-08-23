fa7dff024a81f6ccfa64c3fed375f1f747e5e3d7
CybersecurityRahbar <alghfryloberalghwly@gmail.com>
2026-08-24 00:13:53 +0300

    fix: expose composed dependencies through AppScope

--- lib/app/app_scope.dart
+++ lib/app/app_scope.dart
@@ -1,22 +1,31 @@
 import 'package:flutter/widgets.dart';
 
-import 'environment.dart';
 import '../core/errors/diagnostics.dart';
+import 'app_dependencies.dart';
+import 'environment.dart';
 
 final class AppScope extends InheritedWidget {
   const AppScope({
     required this.environment,
+    required this.dependencies,
     required this.diagnostics,
     required super.child,
     super.key,
   });
+
   final AppEnvironment environment;
+  final AppDependencies dependencies;
   final DiagnosticsReporter diagnostics;
 
-  static AppScope of(BuildContext context) =>
-      context.dependOnInheritedWidgetOfExactType<AppScope>()!;
+  static AppScope of(BuildContext context) {
+    final AppScope? scope = context.dependOnInheritedWidgetOfExactType<AppScope>();
+    assert(scope != null, 'AppScope must be an ancestor of the requested context.');
+    return scope!;
+  }
 
   @override
   bool updateShouldNotify(AppScope oldWidget) =>
-      environment != oldWidget.environment || diagnostics != oldWidget.diagnostics;
+      environment != oldWidget.environment ||
+      dependencies != oldWidget.dependencies ||
+      diagnostics != oldWidget.diagnostics;
 }
