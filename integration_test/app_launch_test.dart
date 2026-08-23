d8fca27e4ad68583c2915f557eda3d6abce1c42e
CybersecurityRahbar <alghfryloberalghwly@gmail.com>
2026-08-24 00:14:17 +0300

    test: adapt integration smoke test to composed lifecycle

--- integration_test/app_launch_test.dart
+++ integration_test/app_launch_test.dart
@@ -1,11 +1,16 @@
+import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
+import 'package:securechat_x/app/app_dependencies.dart';
 import 'package:securechat_x/app/securechat_app.dart';
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
   testWidgets('foundation shell renders', (WidgetTester tester) async {
-    await tester.pumpWidget(const SecureChatApp());
+    final AppDependencies dependencies = AppDependencies.foundation();
+    await tester.pumpWidget(SecureChatApp(lifecycle: dependencies.lifecycle));
+    await tester.pumpAndSettle();
     expect(find.text('SecureChat X'), findsOneWidget);
+    dependencies.dispose();
   });
 }
