2647045459d53048154bd7f2c36661f9a8d5c3a7
CybersecurityRahbar <alghfryloberalghwly@gmail.com>
2026-08-24 17:34:14 +0300

    fix: make contacts shell vertically scrollable

--- lib/features/contacts/contacts_screen.dart
+++ lib/features/contacts/contacts_screen.dart
@@ -12,7 +12,7 @@ class ContactsScreen extends StatelessWidget {
   Widget build(BuildContext context) => SafeArea(
         child: SecureChatConstrained(
           maxWidth: SecureChatContentConstraints.wide,
-          child: Padding(
+          child: SingleChildScrollView(
             padding: SecureChatResponsiveInsets.page(context),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
