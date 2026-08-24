b31c488fbfd573c6eef8f3550a9299d53b0648e0
CybersecurityRahbar <alghfryloberalghwly@gmail.com>
2026-08-25 00:06:27 +0300

    fix: resolve secure storage const analyzer warning

--- lib/core/storage/flutter_secure_secret_store.dart
+++ lib/core/storage/flutter_secure_secret_store.dart
@@ -13,7 +13,7 @@ abstract interface class SecretStorageBackend {
 
 final class FlutterSecureStorageBackend implements SecretStorageBackend {
   FlutterSecureStorageBackend({FlutterSecureStorage? storage})
-    : _storage = storage ?? FlutterSecureStorage();
+    : _storage = storage ?? const FlutterSecureStorage();
 
   final FlutterSecureStorage _storage;
 
